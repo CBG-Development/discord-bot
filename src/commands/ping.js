b68c4fe2d5f37b99e574c25e550b8ad0b5bc5161
Maurice Sonntag <mts.sonntag@gmail.com>
2021-11-18 01:43:44 +0100

    Added comments

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,9 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Interaction } = require('discord.js');
 
+/**
+ * Title: Ping!
+ * Description: Ping the bot :P
+ * Command: ping
+ */
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
+    /**
+     * Execute
+     * @param {Interaction} interaction 
+     */
     async execute(interaction) {
         await interaction.reply('Pong!');
     }
