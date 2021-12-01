50652a55d75ee1566210f0b3175f2c9c951bb1df
Maurice Sonntag <mts.sonntag@gmail.com>
2021-12-01 17:27:54 +0100

    Updated CPoll Command

--- src/commands/cpoll.js
+++ src/commands/cpoll.js
@@ -40,7 +40,7 @@ module.exports = {
          */
         const getAnswerElements = (string) => {
             const emoji = string.split(' ')[0];
-            const regexEmoji = /\p{Extended_Pictographic}/ug;
+            const regexEmoji = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/gi;
             const regexEmojiDiscord = /(<a?)?:\w+:(\d{18}>)?/g;
 
             const answer = string.replace(emoji, '');
@@ -50,7 +50,7 @@ module.exports = {
                     return { 
                         emoji: emoji,
                         answer: answer
-                    }
+                    };
                 } else return null;
             } else return null;
         }
